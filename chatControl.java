d4b06d2cf8d67f088616550fd2b59fe765e3138f
Chi290804 <chihoangf@gmail.com>
2023-12-10 23:52:47 +0700

    new

---
+++ chatControl.java
@@ -0,0 +1,30 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class chatControl {
+
+    @FXML
+    private Button call;
+
+    @FXML
+    private Button chatB;
+
+    @FXML
+    private Button friendB;
+
+    @FXML
+    private Button homeB;
+
+    @FXML
+    private Button info;
+
+    @FXML
+    private Button notiB;
+
+    @FXML
+    private Button outB;
+
+    @FXML
+    private Button video;
+
+}
---
+++ mainControl.java
@@ -0,0 +1,21 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class mainCotrol {
+
+    @FXML
+    private Button chatB;
+
+    @FXML
+    private Button friendB;
+
+    @FXML
+    private Button homeB;
+
+    @FXML
+    private Button notiB;
+
+    @FXML
+    private Button outB;
+
+}
